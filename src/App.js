11e8b37cd8d3a59aa93705cfceb67e0531a734b7
Ace <ace@bellina.com>
2021-09-21 22:46:54 -0700

    Simple list view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Header from './components/header.js'
 import Modules from './components/modules.js'
+import ListView from './list-view/list-view.js'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Split from 'react-split'
 
 function App() {
+
+  let recs = [
+    {Table: "User", Field: "First Name", Type: "String" },
+    {Table: "User", Field: "Last Name", Type: "String" },
+    {Table: "User", Field: "Phone", Type: "String" },
+    {Table: "User", Field: "Active", Type: "True/False" },
+  ];
+
+  let headers = ['Table', 'Field', 'Type'];
+
   return (
     <>
       <Header />
@@ -18,7 +29,7 @@ function App() {
         </div>
 
         <div>
-          Hello BSN2!
+          <ListView headers={headers} recs={recs} />
         </div>
 
       </Split>
