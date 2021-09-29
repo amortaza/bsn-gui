92ef7f3469e29f664044a75c7a5c21295b517b6b
Ace <ace@bellina.com>
2021-09-28 20:17:16 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from "react"
-import { useSelector } from 'react-redux'
+import './App.css';
+import Header from './components/header.js'
+import Modules from './components/modules.js'
+import ListView from './features/list-view/list-view.js'
+import {ListViewState} from './features/list-view/slice'
 
-import { ListViewState } from './features/list-view/slice'
+import Split from 'react-split'
+
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
-  const listViewState = useSelector( ListViewState )
+  const listviewState = useSelector( ListViewState )
+  // const recs = useSelector((state)=> state.recs)
+  // const headers = useSelector((state)=>state.headers)
+  // const dispatch = useDispatch()
 
+  console.log('******************************** hi');
   return (
-    <div>
-        {/* <Counter /> */}
-        D {listViewState.value}
-    </div>
+    <>
+      <Header />
+
+      <Split className="split">
+     
+        <div>   
+          <Modules/>
+        </div>
+
+        <div>
+          <ListView headers={listviewState.headers} recs={listviewState.recs} />
+        </div>
+
+      </Split>
+    </>
   );
 }
 
